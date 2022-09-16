92e718e3e2b7a170bc9d5b651eb9685e5d29586e
Python Klife <pythonklife@gmail.com>
2022-09-17 02:24:41 +0300

    change sqlite to postgres7  commit

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ Bootstrap(app)
 uri = os.getenv("DATABASE_URL")  # or other relevant config var
 if uri and uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
-# else:
-#     uri = 'sqlite:///blog.db'
+else:
+    uri = 'sqlite:///blog.db'
     
 app.config['SQLALCHEMY_DATABASE_URI'] = uri
 
