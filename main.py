1d17c6cd58a46917b53f696be91f270744228afe
Python Klife <pythonklife@gmail.com>
2022-09-17 01:41:13 +0300

    change sqlite to postgres2  commit

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import os
 
 app = Flask(__name__)
 load_dotenv()
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SECRET_KEY'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
 personal_access_token_GitHub = os.getenv("personal_access_token_GitHub")
 ckeditor = CKEditor(app)
 Bootstrap(app)
